4196cc52280f5b3baeba6d34eec05cd01d3806a7
Amit Znaft <44845542+amitznaft@users.noreply.github.com>
2019-06-11 09:53:42 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,144 +1,200 @@
-/**
- * Examples of automatic tests for the exercise on binary trees.
- *
- * @author Erel Segal-Halevi
- * @since 2019-02
- */
-
 #include <iostream>
-using std::cout, std::endl;
+using namespace std;
 
-#include <iomanip>
-#include "badkan.hpp"
 #include "range.hpp"
 #include "chain.hpp"
 #include "zip.hpp"
 #include "product.hpp"
 #include "powerset.hpp"
+#include "badkan.hpp"
 using namespace itertools;
 
+
 template<typename Iterable>
-string toString(const Iterable& iterable){
- stringstream ostr;
- for(decltype(*(iterable.begin())) i: iterable){
-   ostr << i << ",";
- }     
- return ostr.str();
-}     
-int main() { 
-    //tester
-      badkan::TestCase tc("Tests");
-     tc.setname("Range test");
+string iterable_to_string(const Iterable& iterable) {
+	ostringstream ostr;
+	for (auto i: iterable)
+		ostr << i << ",";
+	return ostr.str();
+}
+
+int main()
+{
+	badkan::TestCase testcase;
+	int grade = 0;
+	int signal = setjmp(badkan::longjmp_buffer);
+	if(signal == 0)
+	{
+		testcase.setname("range tests");
+		{
+			int i = 0;
+			for(int j: range(0,1000))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		{
+			char i = 'a';
+			for(char j: range('a','z'))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		{
+			double i = 1.5;
+			for(double j: range(1.5,2000.5))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		{
+			double i = -500;
+			for(double j: range(-500,500))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		testcase.setname("chain tests");
+		{
+			int i = 0;
+			for(int j: chain(range(0,1000),range(1000,2000)))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		{
+			char i = 'a';
+			for(char j: chain(range('a','p'),range('q','z')))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+
+		{
+			char i = 'a';
+			for(char j: chain(string("abcdefghijk"),string("lmnopqrstuvwxyz")))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		{
+			char i = 'a';
+			for(char j: chain(range('a' , 'k'),string("lmnopqrstuvwxyz")))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		{
+			int i = -1000;
+			for(int j: chain(range(-1000,0),range(0,1000)))
+			{
+				testcase.CHECK_EQUAL(j , i);
+				i++;
+			}
+		}
+		testcase.setname("zip tests");
+		{
+			int i = 0;
+			char j = 'a';
+			for(auto k: zip(range(1,6) , range('a' , 'f')))
+			{
+				testcase.CHECK_OUTPUT(k , to_string(i) + "," + to_string(j));
+				i++;
+				j++;
+			}
+		}
+
+		{
+			int i = 0;
+			int j = 7;
+			for(auto k: zip(range(1,6) , range(7 , 12)))
+			{
+				testcase.CHECK_OUTPUT(k , to_string(i) + "," + to_string(j));
+				i++;
+				j++;
+			}
+		}
+		{
+			int i = 'f';
+			char j = 'a';
+			for(auto k: zip(range('f' , 'k') , range('a' , 'f')))
+			{
+				testcase.CHECK_OUTPUT(k , to_string(i) + "," + to_string(j));
+				i++;
+				j++;
+			}
+		}
+		{
+			char i = 'a';
+			char j = 'h';
+			for(auto k: zip(string("abcdefg") , string("hijklmn")))
+			{
+				testcase.CHECK_OUTPUT(k , to_string(i) + "," + to_string(j));
+				i++;
+				j++;
+			}
+		}
+		{
+			int j = 0;
+			char k = 'a';
+			int l = 11;
+			char m = 'k';
+			for(auto i: zip(zip(range(0,10),string("abcdefghij")),zip(range(11,21) , string("klmnopqrst"))))
+			{
+				testcase.CHECK_OUTPUT(i , to_string(j) + "," + to_string(k) + "," + to_string(l) + "," + to_string(m));
+				j++;
+				k++;
+				l++;
+				m++;
+			}
+		}
+		testcase.setname("product tests");
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(product(range(1,4) , string("hello"))) , "1,h 1,e 1,l 1,l 1,o 2,h 2,e 2,l 2,l 2,o 3,h 3,e 3,l 3,l 3,o");
+		}
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(product(range(1,3) , range(1,3))) , "1,1 1,2 2,1 2,2");
+		}
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(product(string("hi") , string("by"))) , "h,b h,y i,b i,y");
+		}
+		testcase.setname("powerset tests");
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(powerset(range(1,4))) , "{}{1}{2}{1,2}{3}{1,3}{2,3}{1,2,3}");
+			
+		}
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(powerset(chain(range('a','c') , range('x','z') ))) , "{}{a}{b}{a,b}{x}{a,x}{b,x}{a,b,x}{y}{a,y}{b,y}{a,b,y}{x,y}{a,x,y}{b,x,y}{a,b,x,y}");
+			
+		}
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(powerset(string("ab"))) , "{}{a}{b}{a,b}");
+			
+		}
+		{
+			testcase.CHECK_OUTPUT(iterable_to_string(powerset(chain(range('a','c') , range('1','3') ))) , "{}{a}{b}{a,b}{1}{a,1}{b,1}{a,b,1}{2}{a,2}{b,2}{a,b,2}{1,2}{a,1,2}{b,1,2}{a,b,1,2}");
+			
+		}
 
 
-    //Containers:
-
-        //Ranges:
-    range int1 = range(-1,3);
-    range int2 = range(6,7);
-    range double1 = range(0.1,4.1);
-    range char1 = range('a','d');
-    range oneChar = range('a','b');
-    range hugeRangeCheckBytes = range(0,10000000); //should give max of 8 bytes, or anything below 100bytes will be reasonable.
-    //if it takes too much time to compute and construct the object, something is wrong!
-    string str = string("our test");
-        //Chains:
-    chain c1 = chain(int1,int2);
-    chain c2 = chain(str,char1);
-    chain c3 = chain(int1,double1);
-
-    std::ostringstream zip1, zip2, zip3, zip4;
-      for (auto zipped : zip(range(-1, 1), range(-1, 1)))
-        zip1 << zipped << "<>";
-      string zip1Ans = "-1,-1<>0,0<>";
-      for (auto zipped : zip(range('b','e'), range('a', 'b')))
-        zip2 << zipped << "<>";
-      string zip2Ans = "b,a<>c,b<>d,c<>";
-      for (auto zipped : zip(string("test"), range(0, 3)))
-        zip3 << zipped << "<>";
-      string zip3Ans = "t,0<>e,1<>s,2<>t,3<>";
-      for (auto zipped : zip(zip(range(0,1),range(3,4)), zip(range('a','c'),range('b','d'))))
-        zip4 << zipped << "<>";
-      string zip4Ans = "0,3,a,b<>";
-
-      std::ostringstream prod1, prod2, prod3, prod4, prod5;
-      for (auto product : product(range(-1, 1), range(-1, 1)))
-        prod1 << product << "*";
-      string prod1Ans = "-1,-1*-1,0*0,-1*0,0*";
-      for (auto product : product(range('b','e'), range('a', 'b')))
-        prod2 << product << "*";
-      string prod2Ans = "b,a*c,a*d,a*";
-      for (auto product : product(string("test"), range(0, 3)))
-        prod3 << product << "*";
-      string prod3Ans = "t,0*t,1*t,2*e,0*e,1*e,2*s,0*s,1*s,2*t,0*t,1*t,2*";
-      for (auto product : product(zip(range(0,1),range(3,4)), zip(range('a','c'),range('b','d'))))
-        prod4 << product << "*";
-      string prod4Ans = "0,3,a,b*0,3,b,c*";
-      for (auto product : product(product(zip(range(0,1),range(3,4)), zip(range('a','c'),range('b','d'))),product(string("test"), range(0, 2))))
-        prod5 << product << "*";
-      string prod5Ans = "0,3,a,b,t,0*0,3,a,b,t,1*0,3,a,b,e,0*0,3,a,b,e,1*0,3,a,b,s,0*0,3,a,b,s,1*0,3,a,b,t,0*0,3,a,b,t,1*0,3,b,c,t,0*0,3,b,c,t,1*0,3,b,c,e,0*0,3,b,c,e,1*0,3,b,c,s,0*0,3,b,c,s,1*0,3,b,c,t,0*0,3,b,c,t,1*";
-
-      //   std::ostringstream power;
-      //  for (auto subset : powerset(product(product(zip(range(0,1),range(3,4)), zip(range('a','c'),range('b','d'))),product(string("test"), range(0, 2)))))
-      //     power << subset << "-";
-      //   cout << "\n\n answer: " << power.str();
 
 
-    //Answers
-      //Range answers:
-    string int1Ans = "-1,0,1,2,";
-    string int2Ans = "6,";
-    string double1Ans = "0.1,1.1,2.1,3.1,";
-    string char1Ans = "a,b,c,";
-      //Chain answers:
+		grade = testcase.grade();
+	}
+	else
+	{
+		testcase.print_signal(signal);
+		grade = 0;
+	}
 
-    string c1Ans = "-1,0,1,2,6,";
-    string c2Ans = "o,u,r, ,t,e,s,t,a,b,c,";
-    // string c3Ans = "";
-    //Arrayed Answers
-    string rangeAnswerArr[4] = {int1Ans,int2Ans,double1Ans,char1Ans}; 
-    string chainAnswerArr[2] = {c1Ans,c2Ans};
-      //tc.setname("Container Sizes: ")
-      //Badkan does not have less/greater than function. this is manual test: Container Sizes
-      cout << "Size of huge range, in bytes: " << sizeof(hugeRangeCheckBytes) << endl; //should give max of 8 bytes, or anything below 100bytes will be reasonable.
-      //also, if it takes too much time to compute and construct the object, something is wrong!
-      
-      tc.setname("Ranges:")
-      .CHECK_EQUAL(toString(int1),rangeAnswerArr[0])
-      .CHECK_EQUAL(toString(int2),rangeAnswerArr[1])
-      .CHECK_EQUAL(toString(double1),rangeAnswerArr[2])
-      .CHECK_EQUAL(toString(char1),rangeAnswerArr[3])
-      .CHECK_EQUAL(toString(range('a','b')),string("a,")) //1 char
-
-      .setname("Chains:")
-      .CHECK_EQUAL(toString(c1),chainAnswerArr[0])
-      .CHECK_EQUAL(toString(c2),chainAnswerArr[1]) 
-      .CHECK_EQUAL(toString(chain(oneChar,string("b"))),string("a,b,"))
-      .CHECK_EQUAL(toString(chain(chain(oneChar,string("b")),string("c"))),string("a,b,c,")) //chain of chains of ranges (oneChar is range of 1 char)
-
-      .setname("Zips:")
-      .CHECK_EQUAL(zip1.str(), zip1Ans)
-      .CHECK_EQUAL(zip2.str(), zip2Ans)
-      .CHECK_EQUAL(zip3.str(), zip3Ans)
-      .CHECK_EQUAL(zip4.str(), zip4Ans) //mixed combinations
-
-
-      .setname("Products:")
-      .CHECK_EQUAL(prod1.str(), prod1Ans)
-      .CHECK_EQUAL(prod2.str(), prod2Ans)
-      .CHECK_EQUAL(prod3.str(), prod3Ans)
-      .CHECK_EQUAL(prod4.str(), prod4Ans) //mixed
-      .CHECK_EQUAL(prod5.str(), prod5Ans) //huge mixed
-
-      .setname("Powersets:")
-      .CHECK_EQUAL(toString(powerset(range(-3,2))),string("{},{-3},{-2},{-3,-2},{-1},{-3,-1},{-2,-1},{-3,-2,-1},{0},{-3,0},{-2,0},{-3,-2,0},{-1,0},{-3,-1,0},{-2,-1,0},{-3,-2,-1,0},{1},{-3,1},{-2,1},{-3,-2,1},{-1,1},{-3,-1,1},{-2,-1,1},{-3,-2,-1,1},{0,1},{-3,0,1},{-2,0,1},{-3,-2,0,1},{-1,0,1},{-3,-1,0,1},{-2,-1,0,1},{-3,-2,-1,0,1},")) 
-      .CHECK_EQUAL(toString(powerset(range(-1.1,2.9))),string("{},{-1.1},{-0.1},{-1.1,-0.1},{0.9},{-1.1,0.9},{-0.1,0.9},{-1.1,-0.1,0.9},{1.9},{-1.1,1.9},{-0.1,1.9},{-1.1,-0.1,1.9},{0.9,1.9},{-1.1,0.9,1.9},{-0.1,0.9,1.9},{-1.1,-0.1,0.9,1.9},"))
-      .CHECK_EQUAL(toString(powerset(chain(oneChar,string("b")))),string("{},{a},{b},{a,b},")) //powerset chains
-      .CHECK_EQUAL(toString(powerset(powerset(chain(oneChar,string("b"))))),string("{},{a},{b},{a,b},")) //powerset of powerset, of another container
-      // .CHECK_EQUAL(power.str(), "really cant compile this, check line 81-84. powerset cant work with zip and product.") //error 
-      .print();
-
-      cout<< "After tests: " <<  endl; 
-  
-  cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
+	cout << "your grade is: " << grade << endl;
+	return 0;
 }
